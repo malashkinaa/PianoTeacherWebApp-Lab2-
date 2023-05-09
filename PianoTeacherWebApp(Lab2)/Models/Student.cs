a5ae4f96e5093763a1bb8dccda5ebd2ede7cffe0
Yana Malashkina <jana.malashkina@gmail.com>
2023-05-09 19:29:23 +0200

    Add project files.

---
+++ PianoTeacherWebApp(Lab2)/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace PianoTeacherWebApp_Lab2_.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int GenderId { get; set; }
+		public int Age { get; set; }
+		public string Email { get; set; }
+		public string PhoneNumber { get; set; }
+        public string AdditionalInfo { get; set; }
+	}
+}
