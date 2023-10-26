c6b354572577b0edce0d540efd390bb8422fdb10
Yana Malashkina <jana.malashkina@gmail.com>
2023-10-26 23:42:29 +0200

    Changed style (background pict, colors)

--- PianoTeacherWebApp(Lab2)/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
+++ PianoTeacherWebApp(Lab2)/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
@@ -6001,7 +6001,8 @@
     offset: [0, 8],
     trigger: 'click',
     content: '',
-    template: '<div class="popover" role="tooltip">' + '<div class="popover-arrow"></div>' + '<h3 class="popover-header"></h3>' + '<div class="popover-body"></div>' + '</div>'
+	  template: '<div class="popover" role="tooltip">' + '<div class="popover-arrow"></div>' + '<h3 class="popover-
+	"></h3>' + '<div class="popover - body"></div>' + '</div>'
   };
   const DefaultType$2 = { ...Tooltip.DefaultType,
     content: '(string|element|function)'
